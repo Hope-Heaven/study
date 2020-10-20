9dc103deee5490593663debd21fabe85ab74cc31
Valova Nadiya <Nadenka.va@yandex.ru>
2020-10-20 10:46:07 +0600

    1

---
+++ sa.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class sa 
+{
+public static void main(String[] args) 
+{
+// TODO Auto-generated method stub
+}
+
+{
+	//Scanner in = new Scanner (System.in);
+System.out.println("¬ведите число");
+//int num = in.nextInt();
+//float height = in.nextFloat();
+//System.out.printf("„исло :%d",height);
+//in.close();
+}
+
+}
