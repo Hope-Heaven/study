6a2723ac8ccc775c3d784fa86aefead59270748e
Valova Nadiya <Nadenka.va@yandex.ru>
2020-12-01 11:28:04 +0600

    add new program

---
+++ new/src/a1.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+public class a1 
+{
+	 public static void main(String[] args) 
+	 
+	  {
+	 
+		 Scanner console = new Scanner(System.in);
+		 System.out.println("¬ведите число");
+	      double x = console.nextInt(); //число
+	      double sum = x / 10; //число : 10
+	      System.out.println("x mod 10 = " + x % 10);
+	      int sum ; //
+	      int sum = x + y + z;
+	      if (sum == 3)System.out.println(x);  // if (sum = 3, 6, 9)
+	      else  System.out.println("no");  //иначе вывести текст     
+
+	   }
+}
